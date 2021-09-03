3dc205046712aae18247469acd7d2c7015325706
chihebchabou <chihebchaboutech@gmail.com>
2021-09-03 09:58:02 +0100

    state-challenge

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -2,11 +2,37 @@ import React, { Component } from 'react';
 
 export class ToDoItem extends Component {
   render() {
+    const {
+      todo: { id, task, isComplete },
+      editTask,
+      deleteTask,
+      completeTask,
+    } = this.props;
     return (
       <li>
-        <a href="#!">Learn React</a>
-        <i className="fas fa-edit"></i>
-        <i className="fas fa-trash-alt"></i>
+        <a
+          onClick={() => {
+            completeTask(id);
+          }}
+          style={
+            isComplete ? { textDecoration: 'line-through', color: 'green' } : {}
+          }
+          href="#!"
+        >
+          {task}
+        </a>
+        <i
+          onClick={() => {
+            editTask(id);
+          }}
+          className="fas fa-edit"
+        ></i>
+        <i
+          onClick={() => {
+            deleteTask(id);
+          }}
+          className="fas fa-trash-alt"
+        ></i>
       </li>
     );
   }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,16 +2,82 @@ import React, { Component, Fragment } from 'react';
 import ToDoItem from './ToDoItem';
 
 export class ToDoList extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        task: 'Learn React',
+        isComplete: false,
+      },
+    ],
+    newTask: '',
+  };
+
+  handleChange = (e) => this.setState({ newTask: e.target.value });
+
+  addTask = () => {
+    if (this.state.newTask) {
+      this.setState({
+        todos: [
+          ...this.state.todos,
+          {
+            id: Math.floor(Math.random() * 1000),
+            task: this.state.newTask,
+            isComplete: false,
+          },
+        ],
+      });
+    } else {
+      alert('Please enter a task');
+    }
+  };
+
+  editTask = (id) => {
+    const task = prompt('edit this task');
+    this.setState({
+      todos: this.state.todos.map((todo) =>
+        task && todo.id === id ? { ...todo, task: task } : todo
+      ),
+    });
+  };
+
+  deleteTask = (id) =>
+    this.setState({
+      todos: this.state.todos.filter((todo) => todo.id !== id),
+    });
+
+  completeTask = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) =>
+        todo.id === id ? { ...todo, isComplete: !todo.isComplete } : todo
+      ),
+    });
+  };
+
   render() {
+    const { todos } = this.state;
     return (
       <Fragment>
         <div className="my-todos">
           <h1 className="title">My toDo list</h1>
-          <input type="text" placeholder="Add ToDo..." />
-          <button>Add</button>
+          <input
+            type="text"
+            value={this.state.newTask}
+            onChange={this.handleChange}
+            placeholder="Add ToDo..."
+          />
+          <button onClick={this.addTask}>Add</button>
         </div>
         <ul className="todo-list">
-          <ToDoItem />
+          {todos.map((todo) => (
+            <ToDoItem
+              todo={todo}
+              deleteTask={this.deleteTask}
+              editTask={this.editTask}
+              completeTask={this.completeTask}
+              key={todo.id}
+            />
+          ))}
         </ul>
       </Fragment>
     );
